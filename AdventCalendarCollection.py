b7bf5af20bae11d0befbecd38efeee2125483c76
Omega014 <metallic.code@gmail.com>
2016-12-03 15:49:04 +0000

    create view

--- AdventCalendarCollection.py
+++ AdventCalendarCollection.py
@@ -3,7 +3,7 @@ import os
 
 import ui
 
-from scraper import main
+import scraper
 
 
 class AdventCalendarCollection (object):
@@ -12,7 +12,24 @@ class AdventCalendarCollection (object):
         self.view.background_color = "#fcfcfc"
         self.view.present('fullscreen')
         self.view.name = 'AdventCalendarCollection'
+        self.reload_data(None)
         
-        
+    def refresh(self, sender):
+        scraper.main()
+        self.reload_data(None)
 
+    def reload_data(self, sender):
+        filepath = os.path.join(os.path.realpath('./'), 'registrations.json')
+        with open(filepath, 'r') as f:
+            registrations = json.load(f)
+
+        for data in registrations:
+            day = data['date'].split('-')[-1]
+            if day[0] == '0':
+                day = day[1]
+            title = data['title']
+            self.view['textview'+day].text = title
+            
+                     
+        
 AdventCalendarCollection()
